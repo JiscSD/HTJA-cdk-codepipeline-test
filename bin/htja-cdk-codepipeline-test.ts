4c2e68f1e72133e93912092b2ae419027bdb511b
dan-at-jisc <dan.needham@jisc.ac.uk>
2022-03-07 12:41:07 +0000

    initial commit

---
+++ bin/htja-cdk-codepipeline-test.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MyPipelineStack } from '../lib/htja-cdk-codepipeline-test-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account:  process.env.CDK_DEFAULT_ACCOUNT,
+    region:  process.env.CDK_DEFAULT_REGION,
+  }
+});
+
+app.synth();
